8b6d8c31f2964d7282c92f0f856690934d3ffca7
jhoneybee <854363956@qq.com>
2021-10-23 18:59:37 +0800

    fix: 修复报错信息

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react'
 import { useLocation, Link } from 'react-router-dom'
 import ProLayout from '@ant-design/pro-layout'
-import { Space, Dropdown, Menu, Badge, Tabs } from 'antd'
+import { Space, Dropdown, Menu, Badge, Tabs, Card } from 'antd'
 import { SettingOutlined, BellOutlined } from '@ant-design/icons'
 
 
@@ -41,35 +41,41 @@ interface NotificationProps {
     count: number
 }
 
+const NotificationBody = () => (
+    <>
+        <Tabs
+            className={styles.notificationCard}
+            centered
+        >
+            <Tabs.TabPane
+                tab="消息(4)"
+                key="message"
+                className={styles.notificationTabs}
+            >
+                    消息内容
+            </Tabs.TabPane>
+            <Tabs.TabPane
+                tab="代办(10)"
+                key="task"
+                className={styles.notificationTabs}
+            >
+                    代办内容
+            </Tabs.TabPane>
+        </Tabs><div
+            className={styles.notificationBottom}
+        >
+            <div>清空通知</div>
+            <div>查看更多</div>
+        </div>
+    </>
+)
+
 const Notification: FC<NotificationProps> = ({
     count
 }) => {
     const overlay = (
         <Menu>
-            <Tabs
-                centered
-            >
-                <Tabs.TabPane
-                    tab="消息(4)"
-                    key="message"
-                    className={styles.notificationTabs}
-                >
-                消息内容
-                </Tabs.TabPane>
-                <Tabs.TabPane
-                    tab="代办(10)"
-                    key="task"
-                    className={styles.notificationTabs}
-                >
-                代办内容
-                </Tabs.TabPane>
-            </Tabs>
-            <div
-                className={styles.notificationBottom}
-            >
-                <div>清空通知</div>
-                <div>查看更多</div>
-            </div>
+            <NotificationBody />
         </Menu>
     )
 
@@ -80,12 +86,17 @@ const Notification: FC<NotificationProps> = ({
             placement="bottomLeft"
             overlayClassName={styles.notificationOverlay}
         >
-            <Badge
-                count={count}
-                className={styles.notification}
+            <div
+                className={styles.notificationDropdown}
             >
-                <BellOutlined />
-            </Badge>
+                <Badge
+                    count={count}
+                    className={styles.notification}
+                >
+                
+                    <BellOutlined />
+                </Badge>
+            </div>
         </Dropdown>
     )
 }
