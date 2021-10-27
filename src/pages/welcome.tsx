397cb533d91aa64a282d60864cd3b168674632ae
jhoneybee <854363956@qq.com>
2021-10-27 23:24:21 +0800

    feat: 优化 tabs 的生命周期，添加 open, close, active, status 传递给子组件

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { FC, useRef } from 'react'
+import { Button } from 'antd'
+import { RouteComponentProps } from '@/types'
 
-const App = () => (
-    <>
-        欢迎来到这个页面
-    </>
-)
+
+const App: FC<RouteComponentProps> = ({
+    tabs
+}) => {
+    const count = useRef<number>(0)
+    return  (
+        <>
+            <Button
+                onClick={() => {
+                    count.current += 1
+                    tabs.active({
+                        key: '/',
+                        params: {
+                            message: `hello, word - ${count.current}`
+                        }
+                    })
+                }}
+            >
+                点击跳转到首页
+            </Button>
+            <Button
+                onClick={() => {
+                    count.current += 1
+                    tabs.open({
+                        item: {
+                            key: `newHome${count.current}`,
+                            path: '/',
+                            name: 'New Home Tab'
+                        },
+                        params: {
+                            message: `hello, word New - ${count.current} `
+                        }
+                    })
+                }}
+            >
+                打开新的首页信息
+            </Button>
+        </>
+    )
+}
 
 export default App
