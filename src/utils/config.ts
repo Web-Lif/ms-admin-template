bf2a7e80515e7dde84668310f531370c02cdade0
苯苯 <91561865+robothot@users.noreply.github.com>
2021-10-29 00:23:59 +0800

    fix: 添加标签页的记忆功能 (#12)

---
+++ src/utils/config.ts
@@ -0,0 +1,22 @@
+import Dexie from 'dexie';
+
+
+const db = new Dexie('ms-db');
+
+
+db.version(1).stores({
+    'ms-config': "id,value",
+});
+
+export const getConfigParams = async (name: string) => {
+    const data = await db.table('ms-config').where('id').equals(name).first()
+    return data?.value
+}
+
+export const setConfigParams = async (name: string, value: any) => {
+    await db.table('ms-config').put({
+        id: name,
+        value
+    })
+}
+
