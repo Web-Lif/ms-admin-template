c7e0ef392e8213cc9541d1aaf57f7b77b2384c7e
robothot <854363956@qq.com>
2021-11-13 14:31:46 +0800

    fix: 修复 less 的加载方式

--- .fastpack.ts
+++ .fastpack.ts
@@ -31,7 +31,7 @@ export default getFastpackConfig({
     },
     publicPath: '/ms-template',
     plugins: [
-        new FastpackPluginBabelImport([{"libraryName": "antd",  "style": true, libraryDirectory: 'es' }]),
+        new FastpackPluginBabelImport([{"libraryName": "antd",  "style": true, libraryDirectory: 'es' }] as any),
         new FastpackPluginLessLoader({}),
         new WebpackChainPlugin()
     ],
