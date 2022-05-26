2a8f05bf87688dd95cea9316083d98023786f422
robothot <854363956@qq.com>
2022-05-26 08:53:13 +0800

    fix: 删除错误的type 导入

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { RouteComponentProps } from 'react-router-dom'
 import styles from './styles/notfound.module.less'
 
-const NotFound: React.FC<RouteComponentProps> = () => (
+const NotFound: React.FC = () => (
     <div
         className={styles.notfound}
     />
