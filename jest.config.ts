38edb0c1d00f32c32618b801566061faa3d16d54
robothot <854363956@qq.com>
2022-09-13 15:19:20 +0800

    jest: 修改依赖配置, 去掉 babel 的 jest 配置

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
     // collectCoverageFrom: undefined,
 
     // The directory where Jest should output its coverage files
-    coverageDirectory: "coverage",
+    coverageDirectory: 'coverage',
 
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: [
@@ -137,7 +137,7 @@ export default {
     // snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    testEnvironment: "jsdom",
+    testEnvironment: 'jsdom',
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
@@ -172,7 +172,9 @@ export default {
     // timers: "real",
 
     // A map from regular expressions to paths to transformers
-    // transform: undefined,
+    transform: {
+        '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
@@ -191,4 +193,4 @@ export default {
 
     // Whether to use watchman for file crawling
     // watchman: true,
-};
+}
