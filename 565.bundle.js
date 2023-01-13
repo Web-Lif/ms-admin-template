271b426582981f8c629a01ff2bcd040266cf2b89
robothot <91561865+robothot@users.noreply.github.com>
2023-01-13 08:44:27 +0000

    Deploying to gh-pages from @ Web-Lif/ms-admin-template@686f3de40658edb6c837ffff0c2092aed0e11842 🚀

--- 565.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkms_admin_template=self.webpackChunkms_admin_template||[]).push([[565],{45449:(s,t,a)=>{a.r(t),a.d(t,{default:()=>r});var e=a(85893),n=a(67294);const r=function(s){var t=s.tabs;return(0,n.useEffect)((function(){console.log(JSON.stringify(t.params),t.status)}),[t.status]),(0,e.jsxs)(e.Fragment,{children:["这个是首页信息  ",JSON.stringify(t.params)]})}}}]);
