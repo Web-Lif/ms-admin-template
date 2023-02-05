3d2962c160d1ea3f28496b9c5df11be7c969d37d
robothot <854363956@qq.com>
2023-02-05 12:57:20 +0800

    fix: 修复消息提示的透明问题

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { Dropdown, Badge, Tabs } from '@weblif/fast-ui'
 import { BellOutlined } from '@ant-design/icons'
-import { css, ClassNames } from '@emotion/react'
+import { css } from '@emotion/react'
 
 interface NotificationProps {
     count: number
@@ -9,24 +9,40 @@ interface NotificationProps {
 
 
 const NotificationBody = () => (
-    <>
+    <div
+        css={css`
+            background-color: #fff;
+            border-radius: 4px;
+            box-shadow: 0 6px 16px -8px rgb(0 0 0 / 8%), 0 9px 28px 0 rgb(0 0 0 / 5%), 0 12px 48px 16px rgb(0 0 0 / 3%);
+            width: 336px;
+            height: 450px;
+        `}
+    >
         <Tabs
             centered
             items={[{
                 key: 'message',
-                label: '"消息(4)',
+                label: '消息(4)',
                 children: (
-                    <>
+                    <div
+                        css={css`
+                            margin: 1rem;
+                        `}
+                    >
                         消息内容
-                    </>
+                    </div>
                 )
             },{
                 key: 'task',
-                label: '"代办(10)',
+                label: '代办(10)',
                 children: (
-                    <>
+                    <div
+                        css={css`
+                            margin: 1rem;
+                        `}
+                    >
                         代办内容
-                    </>
+                    </div>
                 )
             }]}
         />
@@ -42,6 +58,7 @@ const NotificationBody = () => (
                     text-align: center;
                     border-top: 1px solid #f0f0f0;
                     border-radius: 0 0 2px 2px;
+                    background-color: #fff;
                     transition: all .3s;
                     &:hover {
                         background-color: #f5f5f5;
@@ -52,7 +69,7 @@ const NotificationBody = () => (
             <div>清空通知</div>
             <div>查看更多</div>
         </div>
-    </>
+    </div>
 )
 
 const Notification: FC<NotificationProps> = ({
@@ -60,36 +77,30 @@ const Notification: FC<NotificationProps> = ({
 }) => {
 
     return (
-        <ClassNames>
-            {({ css: ncss }) => (
-                <Dropdown
-                    trigger={['click']}
-                    placement="bottomLeft"
-                    overlayClassName={ncss`
-                        width: 336px;
-                        height: 500px;
-                    `}
-                    dropdownRender={() => <NotificationBody />}
-                >
-                    <div
-                        css={css`
+        <>
+            <Dropdown
+                trigger={['click']}
+                placement="bottomLeft"
+                dropdownRender={() => <NotificationBody />}
+            >
+                <div
+                    css={css`
                             height: 100%;
                         `}
-                    >
-                        <Badge
-                            count={count}
-                            css={css`
+                >
+                    <Badge
+                        count={count}
+                        css={css`
                                 cursor: pointer;
                                 padding: 4px;
                                 vertical-align: middle;
                             `}
-                        >
-                            <BellOutlined />
-                        </Badge>
-                    </div>
-                </Dropdown>
-            )}
-        </ClassNames>
+                    >
+                        <BellOutlined />
+                    </Badge>
+                </div>
+            </Dropdown>
+        </>
     )
 }
 
